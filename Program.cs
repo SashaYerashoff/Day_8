1bdbf0ed74d84e28befdacd7c2082bae4bcdea4d
SashaYerashoff <a.yerashoff@gmail.com>
2018-05-15 20:14:59 +0300

    Day_8
    Vienkaršie masīvi

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace Day_8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			//-------main------
+			MasivaPiemers();
+			NewStar();
         }
+
+		static void NewStar()
+		{
+			string[] masivs = new string[30];
+
+			string stars = "*";
+			for (int i = 0; i < 30; i++)
+			{
+				
+				masivs[i] = masivs[i] + stars;
+				stars = stars + "*";
+				Console.WriteLine(masivs[i]);
+			}
+		}
+
+
+		static void MasivaPiemers()
+		{
+			int[] masivs = new int[4];
+            masivs[0] = 1;
+            masivs[1] = 5;
+            masivs[2] = 7;
+            masivs[3] = 6;
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine("masīva {0} vērtība ir: {1}", i, masivs[i]);
+            }
+		}
+
+
     }
 }
